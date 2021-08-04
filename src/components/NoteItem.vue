da5f5b951d6b63c0a68413f0fa9e3f2c74024979
Alex <alex.y.kuznetsov@gmail.com>
2021-08-04 18:01:42 +0500

    note

--- src/components/NoteItem.vue
+++ src/components/NoteItem.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="note">Note Item</div>
+  <article class="note">
+    <h1 class="note_title">Note Item</h1>
+    <div class="note_text">Note text is placed here</div>
+    <span class="note_date">12.12.2021</span>
+  </article>
 </template>
 
 <script>
