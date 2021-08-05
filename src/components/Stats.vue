3fdde6897ac1362b60f4ac89e38ebdac670d63bc
Alex <alex.y.kuznetsov@gmail.com>
2021-08-05 20:52:10 +0500

    favorite and marked fix

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="stats">
-    <span class="stats_item">Total: 1</span>
-    <span class="stats_item">Marked: 1</span>
-    <span class="stats_item">Favorite: 1</span>
+    <span class="stats_item">Total: {{ stats.total }}</span>
+    <span class="stats_item">Marked: {{ stats.marked }}</span>
+    <span class="stats_item">Favorite: {{ stats.favorite }}</span>
   </div>
 </template>
 
 <script>
-export default {
+import { mapState } from 'vuex';
 
+export default {
+  computed: {
+    ...mapState([
+      'notes'
+    ]),
+    stats () {
+      return {
+        total: this.notes.length,
+        marked: this.notes.filter(note => note.marked).length,
+        favorite: this.notes.filter(note => note.favorite).length
+      }
+    }
+  }
 }
 </script>
