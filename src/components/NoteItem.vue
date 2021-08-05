e58087e023f8e0c88a0c3965cedb7c250f2d7db3
Alex <alex.y.kuznetsov@gmail.com>
2021-08-05 17:19:15 +0500

    note controls icons

--- src/components/NoteItem.vue
+++ src/components/NoteItem.vue
@@ -3,8 +3,11 @@
     <div class="note_header">
       <h1 class="note_title" v-bind:title="title">{{ title }}</h1>
       <div class="note_status">
-        <span class="note_icon note_new" title="New"></span>
-        <span class="note_icon note_fav" title="Favorite"></span>
+        <input type="checkbox" class="note_input" id="noteMark" />
+        <label class="note_icon note_mark" for="noteMark" title="Mark"></label>
+        <input type="checkbox" class="note_input" id="noteFav" />
+        <label class="note_icon note_fav" for="noteFav" title="Favorite"></label>
+        <button class="note_icon note_delete" title="Delete"></button>
       </div>
     </div>
     <div class="note_text">{{ text }}</div>
--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="stats">
     <span class="stats_item">Total: 1</span>
-    <span class="stats_item">New: 1</span>
+    <span class="stats_item">Marked: 1</span>
     <span class="stats_item">Favorite: 1</span>
   </div>
 </template>
